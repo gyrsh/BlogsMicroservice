601842bbfca448eb1a012c754787a15dad2c995d
gyrsh <gyrshwatson@gmail.com>
2022-12-24 23:25:42 +0530

    Initial Commit

---
+++ src/test/java/com/blogs/blockMicroservice/BlockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogs.blockMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
